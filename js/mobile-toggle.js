7e910622a366fce6b4ceb9b0c3d6980db1bb6207
Tran Tu <tranminhtu.13092004@gmail.com>
2025-10-17 09:24:18 +0700

    Update UI + sửa lỗi responsive + thêm README.md

--- js/mobile-toggle.js
+++ js/mobile-toggle.js
@@ -1,3 +1,9 @@
+
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "../../fonts/fonts-icons.css"; 
+document.head.appendChild(link);
+
 document.addEventListener("DOMContentLoaded", () => {
   const menuLinks = document.querySelectorAll(".mb-menu-link");
 
@@ -25,3 +31,53 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+document.querySelectorAll('.pick').forEach(item => {
+  item.addEventListener('click', () => {
+    item.querySelector('.tick').classList.toggle('active');
+  });
+});
+
+
+
+
+
+
+document.querySelectorAll('.entry_image').forEach(entry => {
+  const cursor = document.createElement('div');
+  cursor.className = 'custom-cursor';
+
+  cursor.innerHTML = `
+    <div class="cursor-icons">
+      <i class="icon-icon-down_arrow-1"></i>
+      <i class="icon-icon-down_arrow-2"></i>
+    </div>
+  `;
+
+  entry.appendChild(cursor);
+
+  function onMove(e) {
+    const rect = entry.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+  }
+
+  entry.addEventListener('mouseenter', e => {
+    entry.classList.add('cursor-visible');
+    entry.addEventListener('mousemove', onMove);
+    onMove(e);
+  });
+
+  entry.addEventListener('mouseleave', () => {
+    entry.classList.remove('cursor-visible', 'cursor-large');
+    entry.removeEventListener('mousemove', onMove);
+  });
+
+  entry.addEventListener('mousedown', () => entry.classList.add('cursor-large'));
+  entry.addEventListener('mouseup', () => entry.classList.remove('cursor-large'));
+});
+
+
